88e2d0709b236b132042767c4a4137b9d396b944
Tony Ramos <ramosaj@usc.edu>
2017-10-21 13:08:49 -0700

    Changed syntax for recipients array

--- react-backend/models/EventSchema.js
+++ react-backend/models/EventSchema.js
@@ -1,12 +1,11 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var RecipientSchema = require("./RecipientSchema");
 
 var EventSchema = new Schema({
     createdBy: String,
     name: String,
     date: String,
-    recipients: [RecipientSchema]
+    recipients: [[{type:mongoose.Schema.Types.ObjectId, ref: 'RecipientSchema'}]]
 
 
 
