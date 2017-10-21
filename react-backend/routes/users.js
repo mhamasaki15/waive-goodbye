1125f31e8e3f08a8a2a37055e03d925296ef357a
Melanie Hamasaki <mhamasak@usc.edu>
2017-10-21 03:08:36 -0700

    initial commit

---
+++ react-backend/routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+//  res.send('respond with a resource');
+
+	res.json([{
+		id: 1,
+		username: "mhamasaki15"
+	}, {
+		id: 2,
+		username: "staceyirawan"
+	}]);
+
+});
+
+module.exports = router;
